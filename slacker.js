45867e8c998c25652904601fa23c0b89d1da5f8e
xluffy <xquang.foss@gmail.com>
2018-09-06 10:30:45 +0700

    Add Eslint, fix some wrong in js file

--- slacker.js
+++ slacker.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
-var moment = require('moment')
+require('dotenv').config();
+
+var moment = require('moment');
 
 function postToSlack(appInfo, submissionStartDate) {
   var WebClient = require('@slack/client').WebClient;
   var client = new WebClient(process.env.SLACK_BOT_API_TOKEN);
 
-  var message = `The status of your app *${appInfo.name}* has been changed to *${appInfo.status}*`
-  var attachment = slackAttachment(appInfo, submissionStartDate)
+  var message = `The status of your app *${appInfo.name}* has been changed to *${appInfo.status}*`;
+  var attachment = slackAttachment(appInfo, submissionStartDate);
   var params = {
-    "attachments" : [attachment],
-    "as_user" : "true"
-  }
+    'attachments' : [attachment],
+    'as_user' : 'true'
+  };
 
   let channel = process.env.SLACK_CHANNEL || '#ios-app-updates';
   client.chat.postMessage(channel, message, params, function(err, res) {
@@ -22,69 +23,69 @@ function postToSlack(appInfo, submissionStartDate) {
 
 function slackAttachment(appInfo, submissionStartDate) {
   var attachment = {
-    "fallback": `The status of your app ${appInfo.name} has been changed to ${appInfo.status}`,
-    "color": colorForStatus(appInfo.status),
-    "title": "iTunes Connect",
-    "author_name": appInfo.name,
-    "author_icon": appInfo.iconUrl,
-    "title_link": `https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/app/${appInfo.appId}`,
-    "fields": [
+    'fallback': `The status of your app ${appInfo.name} has been changed to ${appInfo.status}`,
+    'color': colorForStatus(appInfo.status),
+    'title': 'iTunes Connect',
+    'author_name': appInfo.name,
+    'author_icon': appInfo.iconUrl,
+    'title_link': `https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/app/${appInfo.appId}`,
+    'fields': [
       {
-        "title": "Version",
-        "value": appInfo.version,
-        "short": true
+        'title': 'Version',
+        'value': appInfo.version,
+        'short': true
       },
       {
-        "title": "Status",
-        "value": appInfo.status,
-        "short": true
+        'title': 'Status',
+        'value': appInfo.status,
+        'short': true
       }
     ],
-    "footer": "iTunes Connect",
-    "footer_icon": "https://devimages.apple.com.edgekey.net/app-store/marketing/guidelines/images/app-store-icon.png",
-    "ts": new Date().getTime() / 1000
-  }
+    'footer': 'iTunes Connect',
+    'footer_icon': 'https://devimages.apple.com.edgekey.net/app-store/marketing/guidelines/images/app-store-icon.png',
+    'ts': new Date().getTime() / 1000
+  };
 
   // set elapsed time since "Waiting For Review" start
-  if (submissionStartDate && appInfo.status != "Prepare for Submission" && appInfo.status != "Waiting For Review") {
-    var elapsedHours = moment().diff(moment(submissionStartDate), 'hours')
+  if (submissionStartDate && appInfo.status != 'Prepare for Submission' && appInfo.status != 'Waiting For Review') {
+    var elapsedHours = moment().diff(moment(submissionStartDate), 'hours');
 
-    attachment["fields"].push({
-      "title": "Elapsed Time",
-      "value": `${elapsedHours} hours`,
-      "short": true
-    })
+    attachment['fields'].push({
+      'title': 'Elapsed Time',
+      'value': `${elapsedHours} hours`,
+      'short': true
+    });
   }
 
-  return attachment
+  return attachment;
 }
 
 function colorForStatus(status) {
-  var infoColor = "#8e8e8e"
-  var warningColor = "#f4f124"
-  var successColor1 = "#1eb6fc"
-  var successColor2 = "#14ba40"
-  var failureColor = "#e0143d"
+  var infoColor = '#8e8e8e';
+  var warningColor = '#f4f124';
+  var successColor1 = '#1eb6fc';
+  var successColor2 = '#14ba40';
+  var failureColor = '#e0143d';
 
   var colorMapping = {
-    "Prepare for Submission" : infoColor,
-    "Waiting For Review" : infoColor,
-    "In Review" : successColor1,
-    "Pending Contract" : warningColor,
-    "Waiting For Export Compliance" : warningColor,
-    "Pending Developer Release" : successColor2,
-    "Processing for App Store" : successColor2,
-    "Pending Apple Release" : successColor2,
-    "Ready for Sale" : successColor2,
-    "Rejected" : failureColor,
-    "Metadata Rejected" : failureColor,
-    "Removed From Sale" : failureColor,
-    "Developer Rejected" : failureColor,
-    "Developer Removed From Sale" : failureColor,
-    "Invalid Binary" : failureColor
-  }
+    'Prepare for Submission' : infoColor,
+    'Waiting For Review' : infoColor,
+    'In Review' : successColor1,
+    'Pending Contract' : warningColor,
+    'Waiting For Export Compliance' : warningColor,
+    'Pending Developer Release' : successColor2,
+    'Processing for App Store' : successColor2,
+    'Pending Apple Release' : successColor2,
+    'Ready for Sale' : successColor2,
+    'Rejected' : failureColor,
+    'Metadata Rejected' : failureColor,
+    'Removed From Sale' : failureColor,
+    'Developer Rejected' : failureColor,
+    'Developer Removed From Sale' : failureColor,
+    'Invalid Binary' : failureColor
+  };
 
-  return colorMapping[status]
+  return colorMapping[status];
 }
 
 module.exports = {
