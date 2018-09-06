e43c68e27787549011506ead899a3b3e13c415f6
xluffy <xquang.foss@gmail.com>
2018-09-06 10:31:19 +0700

    Fix follow eslint

--- poll-itc.js
+++ poll-itc.js
@@ -1,44 +1,44 @@
 var slacker = require('./slacker.js');
 var dirty = require('dirty');
 var db = dirty('kvstore.db');
-var pollIntervalSeconds = 120
-var debug = false
+var pollIntervalSeconds = 120;
+var debug = false;
 
 function checkAppStatus() {
-  console.log("Fetching latest app status...")
+  console.log('Fetching latest app status...');
 
   // invoke ruby script to grab latest app status
-  var exec = require("child_process").exec;
+  var exec = require('child_process').exec;
   exec('ruby get-app-status.rb', function (err, stdout, stderr) {
     if (stdout) {
       // compare new app info with last one (from database)
-      console.log(stdout)
+      console.log(stdout);
       var versions = JSON.parse(stdout);
 
       // use the live version if edit version is unavailable
-      var currentAppInfo = versions["editVersion"] ? versions["editVersion"] : versions["liveVersion"];
+      var currentAppInfo = versions['editVersion'] ? versions['editVersion'] : versions['liveVersion'];
       var lastAppInfo = db.get('appInfo');
 
       if (!lastAppInfo || lastAppInfo.status != currentAppInfo.status || debug) {
         slacker.slack(currentAppInfo, db.get('submissionStart'));
 
         // store submission start time
-        if (currentAppInfo.status == "Waiting For Review") {
+        if (currentAppInfo.status == 'Waiting For Review') {
           db.set('submissionStart', new Date());
         }
       }
       else if (currentAppInfo) {
-        console.log(`Current status \"${currentAppInfo.status}\" matches previous status`);
+        console.log(`Current status "${currentAppInfo.status}" matches previous status`);
       }
       else {
-        console.log("Could not fetch app status");
+        console.log('Could not fetch app status');
       }
 
       // store latest app info in database
       db.set('appInfo', currentAppInfo);
     }
     else {
-      console.log("There was a problem fetching the status of the app!");
+      console.log('There was a problem fetching the status of the app!');
     }
   });
 }
